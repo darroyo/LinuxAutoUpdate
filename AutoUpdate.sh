8b1bca0379b4369428fe84c53310b2caf9259fb1
Daniel <danielarco.7@gmail.com>
2025-11-19 09:26:16 +0100

    cambios finales

--- AutoUpdate.sh
+++ AutoUpdate.sh
@@ -1,36 +1,37 @@
 #!/usr/bin/env bash
 
+APT=0
+FLATPAK=0
+SNAP=0
 MSG=""
 
 # --- Comprobar APT (.deb) ---
 if command -v apt >/dev/null 2>&1; then
-    sudo apt update >/dev/null 2>&1
-    UPGRADABLE=$(apt list --upgradable 2>/dev/null | tail -n +2 | wc -l)
-    # if [ "$UPGRADABLE" -gt 0 ]; then
-        MSG+=".DEB: $UPGRADABLE actualización pendiente.\n"
-    # fi
+    sudo /usr/bin/apt update -qq >/dev/null 2>&1
+    APT=$(apt list --upgradable 2>/dev/null | tail -n +2 | wc -l)
+    if [ "$APT" -gt 0 ]; then
+        MSG+=".DEB: $APT actualizaciones pendientes.\n"
+    fi
 fi
 
 # --- Comprobar Flatpak ---
 if command -v flatpak >/dev/null 2>&1; then
     # Quita líneas vacías y cuenta solo las que contengan un ID de app/runtime
-    FUPDATES=$(flatpak remote-ls --updates 2>/dev/null | awk 'NF>0' | wc -l)
-    # if [ "$FUPDATES" -gt 0 ]; then
-        MSG+="FLATPAK $FUPDATES actualización pendiente.\n"
-    # fi
+    FLATPAK=$(flatpak remote-ls --updates 2>/dev/null | awk 'NF>0' | wc -l)
+    if [ "$FLATPAK" -gt 0 ]; then
+        MSG+="FLATPAK $FLATPAK actualizaciones pendientes.\n"
+    fi
 fi
 
 # --- Comprobar Snap ---
 if command -v snap >/dev/null 2>&1; then
     # Lista solo los snaps que tienen actualización disponible
-    SUPDATES=$(snap refresh --list 2>/dev/null | tail -n +2 | wc -l)
+    SNAP=$(snap refresh --list 2>/dev/null | tail -n +2 | wc -l)
     # Si hay cabecera que no quieres contar, cambia tail -n +2
-    #if [ "$SUPDATES" -gt 0 ]; then
-        MSG+="SNAP $SUPDATES actualización pendiente.\n"
-    #fi
+    if [ "$SNAP" -gt 0 ]; then
+        MSG+="SNAP $SNAP actualizaciones pendientes.\n"
+    fi
 fi
 
-# --- Notificación si hay algo ---
-#if [ -n "$MSG" ]; then
-    notify-send "Actualizaciones disponibles" "$MSG"
-#fi
+TOTAL=$((APT + FLATPAK + SNAP))
+notify-send "Actualizaciones: $TOTAL" "$MSG"
